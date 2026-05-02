eb6f9aa9ebd8862eb525b4adc2c767ca70e40ecd
Hannes Börner <hannes15@t-nline.de>
2026-05-02 18:18:21 +0200

    added device buy

--- DDOSClicker/Assets/Scripts/GameManager.cs
+++ DDOSClicker/Assets/Scripts/GameManager.cs
@@ -1,11 +1,13 @@
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Rendering.Universal;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
+    public enum devices { PC, Fridge, Server }
     public static GameManager Instance { get; private set; }
 
     private void Awake()
@@ -28,13 +30,28 @@ public class GameManager : MonoBehaviour
 
     public float manualRpS = 0f;
 
+    public Dictionary<devices, int> boughtDevices = new Dictionary<devices, int>();
+
     private void FixedUpdate()
     {
-        if (lastCalc + 0.2f < Time.time) {
-            manualRpS = clicks.FindAll(x => x > lastCalc -1).Count;
+        if (lastCalc + 0.2f < Time.time)
+        {
+            manualRpS = clicks.FindAll(x => x > lastCalc - 1).Count;
             clicks = clicks.FindAll(x => x > lastCalc - 1);
             lastCalc = Time.time;
         }
-        
+
+    }
+
+    public void BuyDevice(devices device, int amount = 1)
+    {
+        if (boughtDevices.ContainsKey(device))
+        {
+            boughtDevices[device] += amount;
+        }
+        else
+        {
+            boughtDevices.Add(device, amount);
+        }
     }
 }
