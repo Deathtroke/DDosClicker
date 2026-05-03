71ebb47f846657aa90b4de8acde976cc3f1d75e3
Hannes Börner <hannes15@t-nline.de>
2026-05-03 16:37:31 +0200

    fix for build

--- DDOSClicker/Assets/Scripts/EconomyManager.cs
+++ DDOSClicker/Assets/Scripts/EconomyManager.cs
@@ -1,8 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using Unity.Mathematics;
-using Unity.VisualScripting;
-using UnityEditor.Rendering.Universal;
 using UnityEngine;
 using static GameManager;
 
--- DDOSClicker/Assets/Scripts/GameManager.cs
+++ DDOSClicker/Assets/Scripts/GameManager.cs
@@ -1,8 +1,5 @@
 using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting;
-using UnityEditor.Rendering.Universal;
-using UnityEditor.Search;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
